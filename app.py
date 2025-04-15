87c4d9c3cd68acd752ab34f8d25f14c54f29ecfa
Rengapriyaa <159158025+Rengapriyaa@users.noreply.github.com>
2025-04-15 21:07:08 +0530

    Update1 app.py

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def update_song(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # This will create the table(s) if not exist
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
