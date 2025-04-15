b8da98a36576a900ff64937dbb39974e2ae9a532
Rengapriyaa <rengapriyaa@student.tce.edu>
2025-04-15 20:30:46 +0530

    Initial commit of music app

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs.db'
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    artist = db.Column(db.String(100))
+    album = db.Column(db.String(100))
+    genre = db.Column(db.String(50))
+
+@app.route('/')
+def index():
+    songs = Song.query.all()
+    return render_template('index.html', songs=songs)
+
+@app.route('/add', methods=['POST'])
+def add_song():
+    song = Song(
+        title=request.form['title'],
+        artist=request.form['artist'],
+        album=request.form['album'],
+        genre=request.form['genre']
+    )
+    db.session.add(song)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete_song(id):
+    song = Song.query.get(id)
+    db.session.delete(song)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update_song(id):
+    song = Song.query.get(id)
+    song.title = request.form['title']
+    song.artist = request.form['artist']
+    song.album = request.form['album']
+    song.genre = request.form['genre']
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
